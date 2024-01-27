4397f7f5522a332e2ef8ad70948ccead91cd1859
ms-nirmal <nirmalicaniwill@gmail.com>
2024-01-27 12:04:39 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
     <div className="App">
       <Header/>
         <Routes>
-          <Route path="/" element={<Home/>}/>
+          <Route path="/home" element={<Home/>}/>
           <Route path="/tour-packages" element={<Destinations/>}>
           
           </Route>  
